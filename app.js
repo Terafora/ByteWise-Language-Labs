b0760c7f39fd5a58a55d0862c645c1330d3b2a6f
Charlotte Stone <charlie.stone649@gmail.com>
2024-03-01 12:28:12 +0000

    Edit for forms to work with js file

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#myForm').submit(function(e) {
+    $('.myForm').submit(function(e) {
         e.preventDefault();
 
         $.ajax({
@@ -9,31 +9,51 @@ $(document).ready(function() {
             dataType: 'json',
             success: function() {
                 $('#formResult').text('Thank you for your message!');
-                $('#myForm').trigger("reset");
+                $('.myForm').trigger("reset");
             },
             error: function(message, status, error) {
                 var errorMessage = message.status + ': ' + message.statusText
-                $('#formResult').text('Error - ' + errorMessage);
+                $('.formResult').text('Error - ' + errorMessage);
             }
         });
     });
-});
 
-// MailChimp Script
+    $('.myFormTwo').submit(function(e) {
+        e.preventDefault();
 
-function($) {window.fnames = new Array();
-    window.ftypes = new Array();
-    fnames[0]='EMAIL';
-    ftypes[0]='email';
-    fnames[1]='FNAME';
-    ftypes[1]='text';
-    fnames[2]='LNAME';
-    ftypes[2]='text';
-    fnames[3]='ADDRESS';
-    ftypes[3]='address';
-    fnames[4]='PHONE';
-    ftypes[4]='phone';
-    fnames[5]='BIRTHDAY';
-    ftypes[5]='birthday';
-}
-(jQuery));var $mcj = jQuery.noConflict(true);
+        $.ajax({
+            url: $(this).attr('action'),
+            method: 'POST',
+            data: $(this).serialize(),
+            dataType: 'json',
+            success: function() {
+                $('#formResultTwo').text('Thank you for your message!');
+                $('.myForm').trigger("reset");
+            },
+            error: function(message, status, error) {
+                var errorMessage = message.status + ': ' + message.statusText
+                $('.formResultTwo').text('Error - ' + errorMessage);
+            }
+        });
+    });
+
+    // MailChimp Script
+
+    function($) {window.fnames = new Array();
+        window.ftypes = new Array();
+        fnames[0]='EMAIL';
+        ftypes[0]='email';
+        fnames[1]='FNAME';
+        ftypes[1]='text';
+        fnames[2]='LNAME';
+        ftypes[2]='text';
+        fnames[3]='ADDRESS';
+        ftypes[3]='address';
+        fnames[4]='PHONE';
+        ftypes[4]='phone';
+        fnames[5]='BIRTHDAY';
+        ftypes[5]='birthday';
+    }
+    (jQuery));var $mcj = jQuery.noConflict(true);
+
+
