3da6b31604b3c0acea7405260cb9855e38d69182
Charlotte Stone <144109245+Terafora@users.noreply.github.com>
2024-03-07 09:56:14 +0000

    Added fade in animations for elements on site

--- app.js
+++ app.js
@@ -36,4 +36,21 @@ $(document).ready(function() {
             }
         });
     });
+});
 
+// animate on scroll
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach(el => {
+    observer.observe(el);
+});
